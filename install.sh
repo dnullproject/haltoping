a4f110b7249a73aa632f92d93e43c685d99728e5
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2024-06-06 08:53:00 +0300

    timer init
    Signed-off-by: Bohdan Sukhomlinov <shellshock1953@gmail.com>

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 
 SOURCE_DIR="$(dirname "$(readlink -f "$0")")"
 SCRIPT_DEST="/usr/local/bin/haltoping"
-SERVICE_DEST="/etc/systemd/system/haltoping.service"
+SERVICE_DEST="/etc/systemd/system/"
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
@@ -18,11 +18,16 @@ fi
 
 cp "$SOURCE_DIR/haltoping" "$SCRIPT_DEST"
 chmod +x "$SCRIPT_DEST"
-cp "$SOURCE_DIR/haltoping.service" "$SERVICE_DEST"
-sed -i "s|ExecStart=.*|ExecStart=/usr/local/bin/haltoping ${ROUTER_IP}|g" "$SERVICE_DEST"
+
+echo "Installing haltoping service..."
+cp "$SOURCE_DIR/haltoping.timer" "${SERVICE_DEST}/"
+cp "$SOURCE_DIR/haltoping.service" "${SERVICE_DEST}/"
+sed -i "s|ExecStart=.*|ExecStart=/usr/local/bin/haltoping ${ROUTER_IP}|g" "${SERVICE_DEST}/haltoping.service"
 
 systemctl daemon-reload
-systemctl enable haltoping.service
-systemctl restart haltoping.service &
+systemctl disable haltoping.service
+systemctl enable haltoping.timer
+systemctl restart haltoping.service
+systemctl status haltoping.service
 
 echo "haltoping has been installed and started successfully."
